f249215895f8a010858e258ebb272f835b085fc7
Jared <jared.trejo@axity.com>
2023-10-09 12:02:08 -0600

    Title: Subiendo proyecto ✅

---
+++ src/webparts/cardExpertsCentralAxity/components/ICardExpertsCentralAxityProps.ts
@@ -0,0 +1,7 @@
+export interface ICardExpertsCentralAxityProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
